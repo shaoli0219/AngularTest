a3df3f5c20fd4b1d4375a49f48635410dc8174a9
shaoli <shaolisxxa@qq.com>
2019-04-11 10:46:22 +0800

    函数

--- js/jsTest/函数/函数的定义和调用/demo01.js
+++ js/jsTest/函数/函数的定义和调用/demo01.js
@@ -1,6 +1,7 @@
-// 函数定义方法一：
+// 函数体内部的语句在执行时，一旦执行到return时，函数就执行完毕，并将结果返回.如果没有return语句，函数执行完毕后也会返回结果，只是结果为undefined。
 
-function abs(x) {
+// 函数定义方法一：函数声明
+function abs1(x) {
     if (x >= 0) {
         return x;
     } else {
@@ -8,12 +9,8 @@ function abs(x) {
     }
 }
 
-abs(10, 'blablabla'); // 返回10
-abs(-9, 'haha', 'hehe', null); // 返回9
-abs(); // 返回NaN
-
-// 函数定义方法二：
-var abs1 = function (x) {
+// 函数定义方法二：函数表达式
+var abs2 = function (x) {
     if (x >= 0) {
         return x;
     } else {
@@ -21,17 +18,38 @@ var abs1 = function (x) {
     }
 };
 
+// 由于JavaScript允许传入任意个参数而不影响调用，因此传入的参数比定义的参数多也没有问题，虽然函数内部并不需要这些参数：
+abs2(10, 'blablabla'); // 返回10
+abs2(-9, 'haha', 'hehe', null); // 返回9
+// 传入的参数比定义的少也没有问题：
+abs2(); // 返回NaN
+
+
+function abs(x) {
+    if (typeof x !== 'number') {
+        throw 'Not a number';
+    }
+    if (x >= 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+
+console.log(abs(1));
+
 
 function foo(x) {
     console.log('x = ' + x); // 10
-    for (var i=0; i<arguments.length; i++) {
+    for (var i = 0; i < arguments.length; i++) {
         console.log('arg ' + i + ' = ' + arguments[i]); // 10, 20, 30
     }
 }
+
 foo(10, 20, 30);
 
 
-function abs2() {
+function abs3() {
     if (arguments.length === 0) {
         return 0;
     }
@@ -39,6 +57,40 @@ function abs2() {
     return x >= 0 ? x : -x;
 }
 
-abs2(); // 0
-abs2(10); // 10
-abs2(-9); // 9
+abs3(); // 0
+abs3(10); // 10
+abs3(-9); // 9
+
+
+/**
+ * rest参数,由于JavaScript函数允许接收任意个参数，于是我们就不得不用arguments来获取所有参数：
+ * @param a
+ * @param b
+ */
+function restTest1(a, b) {
+    var i, rest = [];
+    if (arguments.length > 2) {
+        for (i = 2; i<arguments.length; i++) {
+            rest.push(arguments[i]);
+        }
+    }
+    console.log('a = ' + a);
+    console.log('b = ' + b);
+    console.log(rest);
+}
+
+restTest1(1, 2, 3, 4, 5);
+
+/**
+ * ES6标准引入了rest参数，上面的函数可以改写为：
+ * @param a
+ * @param b
+ * @param rest
+ */
+function restTest2(a, b, ...rest) {
+    console.log('a = ' + a);
+    console.log('b = ' + b);
+    console.log(rest);
+}
+
+restTest2(1, 2, 3, 4, 5);
---
+++ js/jsTest/函数/变量作用域与解构赋值/解构赋值.js
@@ -0,0 +1,74 @@
+// 传统做法如何把一个数组的元素分别赋值给几个变量：
+
+// var array = ['hello', 'JavaScript', 'ES6'];
+// var x = array[0];
+// var y = array[1];
+// var z = array[2];
+
+
+// 在ES6中，可以使用解构赋值，直接对多个变量同时赋值：
+
+// 如果浏览器支持解构赋值就不会报错:
+// var [x, y, z] = ['hello', 'JavaScript', 'ES6'];
+
+
+// 如果数组本身还有嵌套，也可以通过下面的形式进行解构赋值，注意嵌套层次和位置要保持一致：
+
+// let [x, [y, z]] = ['hello', ['JavaScript', 'ES6']];
+// console.log('x = ' + x + ', y = ' + y + ', z = ' + z);
+
+// 解构赋值还可以忽略某些元素：
+
+let [, , z] = ['hello', 'JavaScript', 'ES6']; // 忽略前两个元素，只对z赋值第三个元素
+console.log('z = ' + z);
+
+// 如果需要从一个对象中取出若干属性，也可以使用解构赋值，便于快速获取对象的指定属性：
+
+// var person = {
+//     name: '小明',
+//     age: 20,
+//     gender: 'male',
+//     passport: 'G-12345678',
+//     school: 'No.4 middle school'
+// };
+// var {name, age, passport} = person;
+//
+// console.log('name = ' + name + ', age = ' + age + ', passport = ' + passport);
+
+
+// 对一个对象进行解构赋值时，同样可以直接对嵌套的对象属性进行赋值，只要保证对应的层次是一致的：
+// var person = {
+//     name: '小明',
+//     age: 20,
+//     gender: 'male',
+//     passport: 'G-12345678',
+//     school: 'No.4 middle school',
+//     address: {
+//         city: 'Beijing',
+//         street: 'No.1 Road',
+//         zipcode: '100001'
+//     }
+// };
+// var {name, address: {city, zipcode}} = person;
+//
+// console.log('name = ' + name + ', city = ' + city + ', zipcode = ' + zipcode);
+// name; // '小明'
+// city; // 'Beijing'
+// zip; // undefined, 因为属性名是zipcode而不是zip
+// // 注意: address不是变量，而是为了让city和zip获得嵌套的address对象的属性:
+// address; // Uncaught ReferenceError: address is not defined
+
+// 使用解构赋值对对象属性进行赋值时，如果对应的属性不存在，变量将被赋值为undefined，这和引用一个不存在的属性获得undefined是一致的。如果要使用的变量名和属性名不一致，可以用下面的语法获取：
+
+var person = {
+    name: '小明',
+    age: 20,
+    gender: 'male',
+    passport: 'G-12345678',
+    school: 'No.4 middle school'
+};
+
+// 把passport属性赋值给变量id:
+var {name, age: myAge, gender: sex, passport: id, school} = person;
+
+console.log('name = ' + name + ', id = ' + id + ', myAge = ' + myAge + ', sex = ' + sex);

3c8f8b33e1a721c7117f9ff1b7c84b57d3fd6c8f
shaolii <shaolisxxa@qq.com>
2019-04-12 15:45:20 +0800

    高阶函数

---
+++ js/jsTest/函数/高阶函数/高阶函数.js
@@ -0,0 +1,11 @@
+// JavaScript的函数其实都指向某个变量。
+// 既然变量可以指向函数，函数的参数能接收变量，那么一个函数就可以接收另一个函数作为参数，这种函数就称之为高阶函数。
+
+'use strict';
+
+function add(x, y, f) {
+    return f(x) + f(y);
+}
+
+var x = add(-5, 6, Math.abs);
+console.log(x);

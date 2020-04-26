e1f6e71b3c15a80f116570e4e2735c8f68b92f8c
shaoli <shaolisxxa@qq.com>
2020-04-26 14:26:02 +0800

    modify

--- js/node.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<head><meta charset="utf-8"/></head>');
-    res.end('你好\n');
-}).listen(1337, "127.0.0.1");
-console.log("Server running at http://127.0.0.1:1337/");
